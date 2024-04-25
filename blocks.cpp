a7e5258824c3c80d3394bfb4b4364ce154b0ef92
Mohamed_Ali <Mohamed811@aucegypt.edu>
2024-04-25 21:34:38 +0200

    colours and design

--- blocks.cpp
+++ blocks.cpp
@@ -1,4 +1,17 @@
 #include "blocks.h"
-
+#include <QBrush>
+#include <QPen>
+#include <cstdlib>
+#include <QColor>
+#include <QVector>
 blocks::blocks() {
+    QVector<QColor> colors={Qt::cyan, Qt::yellow, Qt::blue, Qt::magenta, Qt::gray, Qt::green};
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(colors[rand()%6]);
+    setBrush(brush);
+    QPen pen;
+    pen.setWidth(1);
+    pen.setColor(Qt::black);
+    setPen(pen);
 }
--- slider.cpp
+++ slider.cpp
@@ -1,7 +1,17 @@
 #include "slider.h"
 #include "QKeyEvent"
+#include <QBrush>
+#include <QPen>
 slider::slider() {
     setRect(0, 0,100, 20);
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::black);
+    setBrush(brush);
+    QPen pen;
+    pen.setWidth(1);
+    pen.setColor(Qt::black);
+    setPen(pen);
 }
 void slider::keyPressEvent(QKeyEvent * event){
     if(event->key()== Qt::Key_Left)
