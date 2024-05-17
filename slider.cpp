adb0cbc9912be30fdb38b5ab098eb93ddaa1328a
Youssef_Ayman_Hassan <159152639+Youssef-ayman55@users.noreply.github.com>
2024-05-17 19:44:32 +0300

    Added speed boost and modified fireball

--- slider.cpp
+++ slider.cpp
@@ -18,6 +18,7 @@ slider::slider() {
     timer->start(3);
     right = false;
     left = false;
+    v = 1;
 }
 void slider::keyPressEvent(QKeyEvent * event){
     if(event->type() == QEvent::KeyPress){
@@ -33,6 +34,7 @@ void slider::keyPressEvent(QKeyEvent * event){
         }
         else if(event->key()== Qt::Key_Space) emit start();
         else if(event->key()== Qt::Key_Z) emit fire();
+        else if(event->key() == Qt::Key_Shift) v = 2;
         else if(event->key()== Qt::Key_X){
             if(!slider_extended)emit extends();
             slider_extended=true;
@@ -49,16 +51,17 @@ void slider::keyReleaseEvent(QKeyEvent * event){
         {
             right = false;
         }
+        else if(event->key() == Qt::Key_Shift) v = 1;
     }
 }
 
 void slider::move(){
     if(left){
-        setPos(x()-1,y());
+        setPos(x()-v,y());
         if(slider_extended) setPos(x()-0.5,y());
     }
     else if(right){
-        setPos(x()+1,y());
+        setPos(x()+v,y());
         if(slider_extended) setPos(x()+0.5,y());
     }
 }
